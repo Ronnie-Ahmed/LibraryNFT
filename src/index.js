70ea601fbaff69a3bcddc94c46020e0aa0deaf93
Ronnie-Ahmed <68500792+Ronnie-Ahmed@users.noreply.github.com>
2023-09-08 17:50:22 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  localWallet,
+  frameWallet,
+} from "@thirdweb-dev/react";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain="mumbai" clientId="d974fd84e15a8fb6456c8c94112b56da">
+    <ThirdwebProvider
+      activeChain="mumbai"
+      clientId="d974fd84e15a8fb6456c8c94112b56da"
+      supportedWallets={[
+        metamaskWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+        localWallet(),
+        frameWallet(),
+      ]}
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
